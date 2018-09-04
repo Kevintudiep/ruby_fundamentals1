27ecdfe47c571f2267d15c4705debd8439d811ca
kevintudiep <kevintudiep@gmail.com>
2018-09-04 16:17:25 -0500

    Second Exercise

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+#First Answer (55$ meal)
+puts 55 * 0.15
+
+#Second Answer
+puts "fifty five dollars" + 9
+
+#Third Answer
+puts "I want to live #{45628*7839} years long!"
+
+#Fourth Answer
+puts (10 < 20 && 30 < 20) || !(10 == 11)
