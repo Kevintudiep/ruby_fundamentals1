a40799fadc84e33cfa77c9913e7cb3f01cd005c1
kevintudiep <kevintudiep@gmail.com>
2018-09-04 16:38:33 -0500

    Changes to Exercise 3

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,11 @@
 puts "What is your name?"
+user_name = gets.chomp
+puts "Hello, #{user_name}"
+
+puts "How old are you?"
+age = gets
+puts "Wow you're #{age}years old?!?!"
+
+puts "Where are you from?"
+location = gets.chomp
+puts "#{location} is a great place to live!"
